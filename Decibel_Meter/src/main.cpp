9dc1be596aaf4460a6759ca6cacd31c8dd8f6b44
Nhatkhongbuon <minhnhathust2002@gmail.com>
2023-09-19 15:41:30 +0700

    V1.2

--- Decibel_Meter/src/main.cpp
+++ Decibel_Meter/src/main.cpp
@@ -6,56 +6,52 @@
 #define SCREEN_HIGH 64
 
 int Sensor = A3;
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /*reset=*/U8X8_PIN_NONE);
-//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /*reset=*/U8X8_PIN_NONE);
-static const unsigned char image_Volup_8x6_bits[] U8X8_PROGMEM = {0x48,0x8c,0xaf,0xaf,0x8c,0x48};
+int Battery = A2;
+double var_sensor;
+double var_batery;
+char buffer[100];
 
-int progress = 0; // progress of the progressbar
-char buffer[32]; // helper buffer to construct a string to be displayed
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /*reset=*/U8X8_PIN_NONE);
 
 void setup()
 {
-  Serial.begin(9600);
-  u8g2.begin();
-
-  pinMode(Sensor, INPUT);
+    Serial.begin(9600);
+    u8g2.begin();
+    pinMode(Sensor, INPUT);
+    pinMode(Battery, INPUT);
 }
 
 void loop()
-{
-  int var = analogRead(Sensor);
-  Serial.println(var);
+{   
+    u8g2.setBitmapMode(1);
+    var_sensor = analogRead(Sensor); // from 0 to 1023
+    var_batery = analogRead(Battery); // from 0 to 1023
 
-  u8g2.clearBuffer();
 
-  u8g2.setBitmapMode(1);
-  u8g2.drawLine(0, 10, 127, 10);
-  u8g2.drawFrame(105, 2, 21, 7);
-  u8g2.drawFrame(103, 4, 2, 3);
-  u8g2.drawFrame(13, 25, 103, 14);
-  // DB do duoc
-  if(var > 30) {
-    u8g2.drawBox(15, 27, var/10 - 3, 10);
-  }
-  else {
-    u8g2.drawBox(15, 27, 0, 10);
-  }
-  // Print Decibel
-  u8g2.setFont(u8g2_font_helvB08_tr);
-  u8g2.drawStr(25, 50, "Decibel: ");
-  // Print number
-  sprintf(buffer, "%d DB", var/10);
-  u8g2.setFont(u8g2_font_helvB08_tr);
-  u8g2.drawStr(71, 50, buffer);
+    u8g2.clearBuffer();
+    u8g2.drawFrame(12, 18, 104, 14);
 
-  u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-  u8g2.drawStr(1, 9, "THCS Que Nham");
-  u8g2.drawXBMP( 92, 3, 8, 6, image_Volup_8x6_bits);
-  // Dung luong Pin con lai
-  u8g2.drawBox(120, 2, 19, 7);
 
-  u8g2.sendBuffer();
+    u8g2.drawBox(14, 20, /*length of decibel bar 0 - 100*/int(var_sensor/10.2), 10);
+    u8g2.drawFrame(102, 2, 24, 6);
 
-  
-  delay(100);
+    int head_batery = int(123 - (var_batery/51.15));
+    int tail_batery = int(3+(var_batery/51.15));
+    u8g2.drawBox(/*head of batery bar (100 - 0)103 - 123*/head_batery, 3, /*tail of batery bar (100 - 0)23 - 3*/tail_batery, 4);
+    u8g2.drawFrame(101, 4, 2, 2);
+
+
+    //Print text
+    u8g2.drawLine(0, 10, 128, 10);
+    u8g2.setFont(u8g2_font_haxrcorp4089_tr);
+    u8g2.drawStr(1, 8, "THCS QUe Nham");
+    
+    sprintf(buffer, "%d DB", int(var_sensor/7.85));
+    u8g2.setFont(u8g2_font_profont22_tr);
+    u8g2.drawStr(47, 49, buffer);
+
+    u8g2.setFont(u8g2_font_haxrcorp4089_tr);
+    u8g2.drawStr(12, 48, "Decibel:");
+
+    u8g2.sendBuffer();
 }
